a68f86128228395944c6a231829bdea6c8c12891
cc32d9 <cc32d9@gmail.com>
2022-01-27 22:21:09 +0000

    ready for tests

---
+++ proton_defi_track_dbsetup.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE proton_defi_track;
+
+CREATE USER 'proton_defi_track'@'localhost' IDENTIFIED BY 'keiH8eej';
+GRANT ALL ON proton_defi_track.* TO 'proton_defi_track'@'localhost';
+grant SELECT on proton_defi_track.* to 'proton_defi_track_ro'@'%' identified by 'proton_defi_track_ro';
+
+use proton_defi_track;
+
+CREATE TABLE SYNC
+(
+ network           VARCHAR(15) PRIMARY KEY,
+ block_num         BIGINT NOT NULL
+) ENGINE=InnoDB;
+
+
+
